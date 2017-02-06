99daf45bba84496c02a57232da0d27c709158bf2
Marchps <marc@highproshop.fr>
2017-02-06 14:28:38 +0100

    update index_ljm

--- app/Views/boutiques_ljm/index.php
+++ app/Views/boutiques_ljm/index.php
@@ -9,22 +9,25 @@
                     </colgroup>
                     <thead>
                         <tr>
-                            <th class="head0">Action</th>
+                            <th class="head0">ID</th>
                             <th class="head0">Nom</th>
                             <th class="head1">Enseigne</th>
                             <th class="head0">Ville</th>
-                            <th class="head1">Etat</th>
+                            <th class="head1">Action</th>
                         </tr>
                     </thead>
                     <tbody>
 
                     <?php foreach ($boutiques_ljm as $btq): ?>
                         <tr class="gradeX">
-                            <td><input type="button" traget="_blank" value="Télécharger" onclick="window.location='http://......./"></td>
+                            <td><?= $btq->id_boutique ?></td>
                             <td><?= $btq->nom ?></td>
                             <td><?= $btq->enseigne ?></td>
                             <td><?= $btq->ville ?></td>
-                            <td class="center"><?= $btq->active ?></td>
+                            <td>
+                                <input type="button" traget="_blank" value="Editer" onclick="<?= BASE_LINK ?>/boutiques/edit/<?= $btq->id_boutique ?>">
+                                <input type="button" traget="_blank" value="Supprimer" onclick="<?= BASE_LINK ?>/boutiques/delete/<?= $btq->id_boutique ?>">
+                            </td>
                         </tr>
                     <?php endforeach; ?>
 
